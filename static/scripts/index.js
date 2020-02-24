c77e3dd86b69e287647b9e41483c96da43a45888
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-23 17:05:45 -0800

    Formatted html and changed submit button selector

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
-    $('.navbar-brand').click(function(){
-        document.location.href = 'index.html'
-    });
-    $('button.btn.submit').submit(function () {
-        $.ajax(
-            {
-                url: `http://0.0.0.0:5000/api/v1/${$('#hb_proj').val()}`,
-                type: 'POST',
-                dataType: 'json',
-                contentType: 'application/json',
-                data: JSON.stringify({
-                    hbtn_user: $('#hb_id').val(),
-                    hbtn_pass: $('#hb_pwd').val(),
-                    hbtn_api_key: $('#api_id').val(),
-                    github_pass: $('#g_pwd').val()
-                }),
-                success: function (response) {
-                    window.location.href = "/done/" + response.body;
-                },
-                error: function (error) {
-                    window.location.href = "/404/";
-                }
-            }
-        );
-    });
+  $('.navbar-brand').click(function(){
+    document.location.href = 'index.html'
+  });
+  $('button.submit').click(function () {
+    $.ajax(
+      {
+        url: `http://0.0.0.0:5000/api/v1/${$('#hb_proj').val()}`,
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          hbtn_user: $('#hb_id').val(),
+          hbtn_pass: $('#hb_pwd').val(),
+          hbtn_api_key: $('#api_id').val(),
+          github_pass: $('#g_pwd').val()
+        }),
+        success: function (response) {
+          window.location.href = "/done/" + response.body;
+        },
+        error: function (error) {
+          window.location.href = "/404/";
+        }
+      }
+    );
+  });
 });
