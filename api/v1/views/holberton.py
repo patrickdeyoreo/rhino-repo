b9994f00b87bfcb24a09eeedec582a5a37b2f0e7
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-23 19:11:46 -0800

    Modified imports in holberton route

--- api/v1/views/holberton.py
+++ api/v1/views/holberton.py
@@ -2,23 +2,17 @@
 """
 Provides RESTful API routes for Holberton
 """
-from os import chdir, getcwd, path
-from re import match
-from shlex import quote, split
-from shutil import rmtree
-from subprocess import run
-from tempfile import mkdtemp
-from flask import abort, jsonify, make_response, request
-from requests import get, post
+import os
+import shlex
+import shutil
+import subprocess
+import tempfile
+import flask
+import requests
 from rhinoscraper import create_session, get_soup, rhinoscrape
 from . import app_views
 
-AUTH_KEYS = {
-    'hbtn_user',
-    'hbtn_pass',
-    'hbtn_api_key',
-    'github_pass'
-}
+AUTH_KEYS = {'hbtn_user', 'hbtn_pass', 'hbtn_api_key', 'github_pass'}
 
 
 @app_views.route("/<project_id>", methods=['POST'])
@@ -31,53 +25,52 @@ def hbtn_project(project_id):
     hbtn_api_key
     github_pass
     """
-    auth = request.get_json()
+    auth = flask.request.get_json()
     if AUTH_KEYS - auth.keys():
-        abort(400)
+        flask.abort(400)
     auth['hbtn_user'] = auth['hbtn_user'].split('@')[0]
     if not auth['hbtn_user'].isnumeric():
-        abort(400)
+        flask.abort(400)
     auth['hbtn_user'] = '@'.join([
         auth['hbtn_user'], 'holbertonschool.com'
     ])
-    auth_token = hbtn_auth_token(
-        auth['hbtn_user'],
-        auth['hbtn_pass'],
-        auth['hbtn_api_key']
+    auth['hbtn_token'] = hbtn_auth_token(
+        auth['hbtn_user'], auth['hbtn_pass'], auth['hbtn_api_key']
     )
-    user = hbtn_user_info(auth_token)
-    project = hbtn_project_info(project_id, auth_token)
-    project_name = project['name']
-    project_repo = project['tasks'][0]['github_repo']
+    user = hbtn_user(auth['hbtn_token'])
+    proj = hbtn_project(project_id, auth['hbtn_token'])
+    repo = proj['tasks'][0]['github_repo']
     with create_session(auth['hbtn_user'], auth['hbtn_pass']) as session:
-        create_project(
-            get_soup(session, project_id),
-            github_user=user['github_user'],
-            github_pass=auth['github_pass'],
-            github_name=user['full_name'],
-            github_repo=project_repo,
-        )
-    return make_response(path.join(project_repo, project_name), 200)
+        new_project(get_soup(session, project_id),
+                    github_user=user['github_user'],
+                    github_pass=auth['github_pass'],
+                    github_name=user['full_name'],
+                    github_repo=repo)
+    return (os.path.join(repo, proj['name']), 200)
 
 
-def create_project(soup, github_user, github_pass, github_repo, github_name):
+def new_project(soup, github_user, github_pass, github_repo, github_name):
     """Scrape project and perform git operations
     """
-    oldcwd = getcwd()
-    tmpdir = mkdtemp()
-    gitdir = path.join(tmpdir, github_repo)
     giturl = 'https://{user}:{password}@github.com/{user}/{repo}.git'.format(
         user=github_user, password=github_pass, repo=github_repo
     )
-    run(split('git clone {} {}'.format(quote(giturl), quote(gitdir))))
-    chdir(gitdir)
+    oldcwd = os.getcwd()
+    tmpdir = tempfile.mkdtemp()
+    gitdir = os.path.join(tmpdir, github_repo)
+    cmd = 'git clone {} {}'.format(shlex.quote(giturl), shlex.quote(gitdir))
+    subprocess.run(shlex.split(cmd), check=False)
+    os.chdir(gitdir)
     rhinoscrape(soup, github_user, github_name)
-    run(split('git add *'), check=False)
-    message = 'Project committed by Rhino Repo'
-    run(split('git commit -m {}'.format(quote(message))))
-    run(split('git push {}'.format(quote(giturl))))
-    chdir(oldcwd)
-    rmtree(tmpdir, ignore_errors=True)
+    cmd = 'git add .'
+    subprocess.run(shlex.split(cmd), check=False)
+    msg = 'Project committed by Rhino Repo'
+    cmd = 'git commit -m {}'.format(shlex.quote(msg))
+    subprocess.run(shlex.split(cmd), check=False)
+    cmd = 'git push {}'.format(shlex.quote(giturl))
+    subprocess.run(shlex.split(cmd), check=False)
+    os.chdir(oldcwd)
+    shutil.rmtree(tmpdir, ignore_errors=True)
 
 
 def hbtn_auth_token(hbtn_user, hbtn_pass, hbtn_api_key):
@@ -90,23 +83,23 @@ def hbtn_auth_token(hbtn_user, hbtn_pass, hbtn_api_key):
         'password': hbtn_pass,
         'scope': 'checker'
     }
-    resp = post(url, params=params)
+    resp = requests.post(url, params=params)
     return resp.json().get('auth_token')
 
 
-def hbtn_user_info(auth_token):
+def hbtn_user(auth_token):
     """Get holberton project info
     """
     url = 'https://intranet.hbtn.io/users/me.json'
     params = {'auth_token': auth_token}
-    resp = get(url, params=params)
+    resp = requests.get(url, params=params)
     return resp.json()
 
 
-def hbtn_project_info(project_id, auth_token):
+def hbtn_project(project_id, auth_token):
     """Get holberton project info
     """
     url = 'https://intranet.hbtn.io/projects/{}.json'.format(project_id)
     params = {'auth_token': auth_token, 'id': project_id}
-    resp = get(url, params=params)
+    resp = requests.get(url, params=params)
     return resp.json()
